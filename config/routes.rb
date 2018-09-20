bc6a1a286eb05287b87a8c8a6f0ab0530ced32da
Кирилл Еропкин <krill221@gmail.com>
2018-09-20 20:07:29 +0300

    heroku

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   end
   post "/graphql", to: "graphql#execute"
   get 'app', to: 'app#index'
+  root to: 'app#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
