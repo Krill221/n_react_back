efb324ddfdfe33d17d8abdd96e51efcc92a29ecc
krill221 <krill221@gmail.com>
2018-10-09 01:44:09 +0300

    task edit

--- app/graphql/mutations/task_mutations/update_task.rb
+++ app/graphql/mutations/task_mutations/update_task.rb
@@ -6,11 +6,13 @@ module Mutations
     argument :body, String, required: true
 
     ## return
-   field :task, Types::TaskType, null: true
+   field :id, ID, null: true
+   field :name, String, null: true
+   field :body, String, null: true
 
    def resolve(id:, name:, body:)
      task = Task.update(id, name: name, body: body)
-     { task: task }
+     { id: task.id, name: task.name, body: task.body }
    rescue ActiveRecord::RecordInvalid => e
      GraphQL::ExecutionError.new("Invalid input: #{e.record.errors.full_messages.join(', ')}")
    end
