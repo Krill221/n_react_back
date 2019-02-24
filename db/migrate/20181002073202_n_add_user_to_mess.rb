16b9ca22de28a7ba497dbf09cb0335735562b933
kirilleropkin <krill221@gmail.com>
2019-02-24 11:57:53 +0300

    migration fix

--- db/migrate/20181002073202_n_add_user_to_mess.rb
+++ db/migrate/20181002073202_n_add_user_to_mess.rb
@@ -1,6 +1,7 @@
 class NAddUserToMess < ActiveRecord::Migration[5.2]
   def change
     remove_foreign_key :messages, :users
+    add_column :user_id, :messages, :integer
     add_reference :messages, :user, foreign_key: true
   end
 end
