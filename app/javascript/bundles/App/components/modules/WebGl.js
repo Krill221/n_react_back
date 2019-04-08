22c446b762cb7af0392729f087557c38d1acaff4
kirilleropkin <krill221@gmail.com>
2019-04-08 22:07:35 +0300

    save and load fron js

--- app/javascript/bundles/App/components/modules/WebGl.js
+++ app/javascript/bundles/App/components/modules/WebGl.js
@@ -4,23 +4,58 @@ import Unity, { UnityContent } from "react-unity-webgl";
 
 export default class WebGl extends React.Component {
 
-  unityContent = new UnityContent(
-      "/assets/draft/webBuild.json",
-      "/assets/draft/UnityLoader.js"
-  );
-
-  fullScreen = () => {
-    this.unityContent.send("Canvas", "InitItem", "Item1|5");
-    this.unityContent.send("Canvas", "InitItem", "Item2|20");
-    this.unityContent.send("Canvas", "PlaceItems", 1);
-
-    //this.unityContent.SetFullscreen(1);
+  constructor(props) {
+      super(props);
+      this.unityContent = new UnityContent(
+          "/assets/draft/webBuild.json",
+          "/assets/draft/UnityLoader.js"
+      );
+      this.state ={
+        itemsCount: 0,
+        items: [],
+        saved: false
+      }
+      this.unityContent.on("onGetFieldItemsCount", count => {
+        this.setState({itemsCount: count, items: []});
+        for(var i=0;i<count;i++){
+          this.unityContent.send("Canvas", "GetItemFromField", i);
+        }
+      });
+      this.unityContent.on("onGetItemFromField", item => {
+        this.setState( (prevState) => {
+          items: prevState.items.push(item)
+        });
+        var arr = item.split('|');
+      });
   }
+
+
+  Save = () => {
+    this.unityContent.send("Canvas", "GetFieldItemsCount");
+    this.setState({saved: true});
+  }
+
+  Load = () => {
+    this.unityContent.send("Canvas", "ClearField");
+    for(var i=0;i<this.state.items.length;i++) {
+      this.unityContent.send("Canvas", "AddItemToField", this.state.items[i]);
+    }
+    this.setState({saved: false});
+  }
+
+  Inits = () => {
+    this.unityContent.send("Canvas", "AddItemToStorage", "Item1|5");
+    this.unityContent.send("Canvas", "AddItemToStorage", "Item2|20");
+    this.unityContent.send("Canvas", "PlaceItems", 1);
+  }
+
 
   render() {
     return <div>
         <Unity unityContent={this.unityContent} />
-        <button className="btn" onClick={ () => this.fullScreen() }>Full Screen</button>
+        <button className="btn" onClick={ () => this.Inits() }>Init Store</button>
+        <button className="btn" onClick={ () => this.Save() }  disabled={this.state.saved}>Save</button>
+        <button className="btn" onClick={ () => this.Load() }  disabled={!this.state.saved}>Load</button>
       </div>;
   }
 }
